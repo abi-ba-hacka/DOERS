2ceae21950fbb70d392c3696d0d68a4c13af4c38
Federico Perez <Federico Perez>
2017-04-08 14:53:59 -0300

    enable cors no

--- app.js
+++ app.js
@@ -13,11 +13,7 @@ const
 var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
