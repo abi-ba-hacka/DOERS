97bdaeb7a700deb4ed2b73448499143aa72710c8
yazkyChristian <christian.yazky@mercadolibre.com>
2017-04-08 16:03:28 -0300

    Se arregla la cagada del point

--- app.js
+++ app.js
@@ -304,7 +304,117 @@ function userAddsItem(senderID, variedad) {
     };
 
 function sendPointList(senderID){
-    sendTextMessage(senderID,"Point");
+   var messageData = {
+        recipient:{
+          id:senderID
+        }, message: {
+          attachment: {
+              type: "template",
+              payload: {
+                  template_type: "list",
+                  elements: [
+                      {
+                          title: "Classic T-Shirt Collection",
+                          image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                          subtitle: "See all our colors",
+                          default_action: {
+                              type: "web_url",
+                              url: "https://peterssendreceiveapp.ngrok.io/shop_collection",
+                              messenger_extensions: true,
+                              webview_height_ratio: "tall",
+                              fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                          },
+                          buttons: [
+                              {
+                                  title: "View",
+                                  type: "web_url",
+                                  url: "https://peterssendreceiveapp.ngrok.io/collection",
+                                  messenger_extensions: true,
+                                  webview_height_ratio: "tall",
+                                  fallback_url: "https://peterssendreceiveapp.ngrok.io/"                        
+                              }
+                          ]
+                      },
+                      {
+                          title: "Classic White T-Shirt",
+                          image_url: "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
+                          subtitle: "100% Cotton, 200% Comfortable",
+                          default_action: {
+                              type: "web_url",
+                              url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                              messenger_extensions: true,
+                              webview_height_ratio: "tall",
+                              fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                          },
+                          buttons: [
+                              {
+                                  title: "Shop Now",
+                                  type: "web_url",
+                                  url: "https://peterssendreceiveapp.ngrok.io/shop?item=100",
+                                  messenger_extensions: true,
+                                  webview_height_ratio: "tall",
+                                  fallback_url: "https://peterssendreceiveapp.ngrok.io/"                        
+                              }
+                          ]                
+                      },
+                      {
+                          title: "Classic Blue T-Shirt",
+                          image_url: "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                          subtitle: "100% Cotton, 200% Comfortable",
+                          default_action: {
+                              type: "web_url",
+                              url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                              messenger_extensions: true,
+                              webview_height_ratio: "tall",
+                              fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                          },
+                          buttons: [
+                              {
+                                  title: "Shop Now",
+                                  type: "web_url",
+                                  url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                                  messenger_extensions: true,
+                                  webview_height_ratio: "tall",
+                                  fallback_url: "https://peterssendreceiveapp.ngrok.io/"                        
+                              }
+                          ]                
+                      },
+                      {
+                          title: "Classic Black T-Shirt",
+                          image_url: "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
+                          subtitle: "100% Cotton, 200% Comfortable",
+                          default_action: {
+                              type: "web_url",
+                              url: "https://peterssendreceiveapp.ngrok.io/view?item=102",
+                              messenger_extensions: true,
+                              webview_height_ratio: "tall",
+                              fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                          },
+                          buttons: [
+                              {
+                                  title: "Shop Now",
+                                  type: "web_url",
+                                  url: "https://peterssendreceiveapp.ngrok.io/shop?item=102",
+                                  messenger_extensions: true,
+                                  webview_height_ratio: "tall",
+                                  fallback_url: "https://peterssendreceiveapp.ngrok.io/"                        
+                              }
+                          ]                
+                      }
+                  ],
+                   buttons: [
+                      {
+                          title: "View More",
+                          type: "postback",
+                          payload: "payload"                        
+                      }
+                  ]  
+              } //payload
+          } //atachment
+        }//message
+     }//data
+
+   callSendAPI(messageData);  
 }
 
 function callSendAPI(messageData) {
@@ -345,7 +455,7 @@ function analyzeMessage(senderID, messageText){
       case "menu":
         askMenu(senderID, 2);
       break;
-      case "point";
+      case "point":
       sendPointList(senderID);
       break;
       default:
