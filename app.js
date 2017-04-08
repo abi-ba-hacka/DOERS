bb066af408dcb8c950c59d41504f281c926d1f49
Federico Perez <Federico Perez>
2017-04-08 19:17:12 -0300

    log de recepi

--- app.js
+++ app.js
@@ -297,15 +297,15 @@ function userStartPostback(senderID, userName){
 
 function sendReceipt(senderID, itemPedidosList) {
   console.log("generando el recibo pa");
-  var total = 0;
+  var total = {total_cost: 0};
   console.log("ASdasdasdsa" + itemPedidosList);
   for(var i = 0; i < itemPedidosList.length; i++)
   {
-        total += itemPedidosList[i].precio;
+        total.total_cost += parseFloat(itemPedidosList[i].precio);
     console.log(itemPedidosList[i].precio);
   }
   //total.total_cost = total;
-  console.log("dio un total de : " + total);
+  console.log("dio un total de : " + total.total_cost);
   var messageData = {
     recipient: {
       id: senderID
