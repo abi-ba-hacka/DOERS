53c3b9893887402e1af5ca4a986e96a7a1553fca
Federico Perez <Federico Perez>
2017-04-08 16:57:13 -0300

    archivos

---
+++ controllers/pedidoController.js
@@ -0,0 +1,12 @@
+var Pedido = require('../daos/pedidoDao');
+var request = require('request');
+config = require('config');
+
+module.exports = {
+	insertPedido: function(pedido){
+							console.log("llame a grabar pedido con : " + JSON.stringify(pedido));
+							Pedido.insertPedido(pedido);
+							console.log("Me traigo los pedidos con : " + JSON.stringify(pedido))
+							Pedido.getPedidoActivoUser(pedido.userId);
+						},
+}
