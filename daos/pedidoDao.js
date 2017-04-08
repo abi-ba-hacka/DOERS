4cd639bb0e081642570b687fc8a3d3492e853469
Federico Perez <Federico Perez>
2017-04-08 16:18:06 -0300

    daodasd2

--- daos/pedidoDao.js
+++ daos/pedidoDao.js
@@ -4,10 +4,10 @@ config = require('config');
 
 module.exports = {
 	insertPedido: function(pedido){
-							console.log("llame a grabar pedido con : " + pedido);
+							console.log("llame a grabar pedido con : " + JSON.stringify(pedido));
 					    	var pedido = new Pedido({
 							    id: pedido.userId,                      
-							    userId: usuario.userId,
+							    userId: pedido.userId,
 							    activo: "S"  
 							  });
 
