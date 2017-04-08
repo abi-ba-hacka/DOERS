953f22fcf7bda570a012c8ba7304db7c6214669f
Federico Perez <Federico Perez>
2017-04-08 16:09:34 -0300

    nico chupa la pija

---
+++ models/pedidoModel.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+//Schema Query
+//***********
+var PedidoSchema = new Schema({
+	id: { type: String },                      
+    userId: { type: String},
+    activo: { type: String},   
+}, { collection : 'Pedido' })
+
+var Pedido = mongoose.model('Pedido', UserSchema);
+
+
+module.exports = Pedido;
