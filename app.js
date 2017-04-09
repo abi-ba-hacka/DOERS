bb3680171ee63dfc49fa1cf412a745f082e73e4d
Federico Perez <Federico Perez>
2017-04-09 02:06:41 -0300

    oiasjdas

--- app.js
+++ app.js
@@ -628,7 +628,7 @@ function analyzeMessage(senderID, messageText){
 
 function receivedPostback(messagingEvent){
       var postBackObject = {};
-      console.log("recibi postback:" + messagingEvent);
+      console.log("recibi postback:" + JSON.stringify(messagingEvent));
       try {
          postBackObject =  JSON.parse(messagingEvent.postback.payload);
       } catch (e) {
