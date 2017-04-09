45b2da6c7e197c4bf26b28b4731b1e6930333053
yazkyChristian <christian.yazky@mercadolibre.com>
2017-04-08 23:58:17 -0300

    Image item

--- controllers/carouselController.js
+++ controllers/carouselController.js
@@ -8,7 +8,7 @@ module.exports = {
 					postbackObject.barId = pubs[i].id;
 					carouselItem.title = pubs[i].name;
 					carouselItem.subtitle = pubs[i].direction + "\n" +  pubs[i].availableTime ;
-					carouselItem.image = pubs[i].image;
+					carouselItem.image_url = pubs[i].image;
 					carouselItem.buttons.push({type:"element_share"});
 					carouselItem.buttons.push({type:"phone_number",title:"Llamar",payload:"+" + pubs[i].phone_number});
 					carouselItem.buttons.push({type: "postback",title: "Ver mas",payload:  JSON.stringify(postbackObject)});
